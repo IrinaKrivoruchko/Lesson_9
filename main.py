db6cde2c0ff6eaa3ce91e88be49ce58656a6c819
Ira <sokurira@gmail.com>
2023-10-23 15:47:54 +0300

    Additionally

--- main.py
+++ main.py
@@ -11,22 +11,17 @@ Devoutly to be wish'd.
 To die, to sleep"""
 
 #1
-# Записываем текст в файл "First.txt"
 with open("First.txt", "w") as first:
     first.write(text_to_write)
 
-# Открываем файл "First.txt" для чтения
 with open("First.txt", "r") as first:
-    # Читаем содержимое файла и разбиваем его на слова
     words = first.read().split()
 
-# Фильтруем слова, чтобы оставить только те, у которых длина не менее 7 букв
 filtered_words = []
 for word in words:
     if len(word) >= 7:
         filtered_words.append(word)
 
-# Открываем файл "Second.txt" для записи и записываем отфильтрованные слова в него
 with open("Second.txt", "w") as second_file:
     second_file.write(" ".join(filtered_words))
 
@@ -34,3 +29,17 @@ with open("Second.txt", "w") as second_file:
 word_count = len(words)
 
 print(f"Count of words in file: {word_count}")
+
+#Additionally
+text = """To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows 
+of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; 
+and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation 
+Devoutly to be wish'd. To die, to sleep"""
+
+word_for_change = "die"
+new_word = "***"
+
+text_after_replace, num_of_replacements = text.replace(word_for_change, new_word), text.count(word_for_change)
+
+print(f"Text after replace: \n{text_after_replace}")
+print(f"Number of replacements: '{word_for_change}': {num_of_replacements}")
